6fb48f43e18e5b730c488e539fd5700a1a099143
AsmaaElgharib <asmaa.samir.elgharib@gmail.com>
2026-05-13 10:54:04 +0300

    Initial clean commit

---
+++ Restaurant.API1/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.OpenApi;
+using Restaurants.API.Middlewares;
+using Serilog;
+
+namespace Restaurants.API.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static void AddPresentation(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthentication();
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Name = "Authorization",
+                Description = "JWT Authorization header"
+            });
+
+            //c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            //{
+            //    {
+            //        new OpenApiSecuritySchemeReference("bearerAuth"),
+            //        new List<string>()
+            //    }
+            //});
+        });
+
+        builder.Services.AddScoped<ErrorHandlingMiddleware>();
+
+        builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
+
+        builder.Host.UseSerilog((context, configuration) =>
+            configuration.ReadFrom.Configuration(context.Configuration)
+        );
+    }
+}
